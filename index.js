7890773a51269d755d3b8e66c156cb94a7592bfc
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2025-04-03 11:47:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const canvas = document.getElementById("canvas");
-let c = canvas.getContext("2d");
-c.imageSmoothingEnabled =false
+let c = canvas.getContext("2d"); //<--bagulhos do canvas
 
-const animal = {
+c.imageSmoothingEnabled = false // liga ou deslica a suavização das imagens do canvas horrivel para pixelart
+
+const animal = { // objeto do animal base
     x:300,
     y:300,
-    vx:0,
-    vy:0,
-    direc:0,
-    direccool:0,
-    wandering:0,
-    wanderingcool:0,
-    direcao:0,
-    energia:1000,
-    maxenergia:1000,
-    agua:1000,
-    aguamaximo:1000,
-    visao:100
-};
+    vx:0, // velocidade no eixo x
+    vy:0, // velocidade no eixo y
+    direc:0, // direção do animal 0,1,2,3 
+    direccool:0, // cooldown para trocar a sua direção 
+    wandering:0, // 1 se o animal esta andando sem rumo,0 se não
+    wanderingcool:0,// cooldown para andar ou parar
+    direcao:0, // se o animal está indo a algum lugar(exemplo comida)
+    energia:1000, // quantidade atual de energia diminui com tempo
+    maxenergia:1000, // maximo de energia ***
+    agua:1000, // quantidade atual de agua diminui com tempo
+    aguamaximo:1000, // maximo de agua ***
+    visao:100, // alcance da visão do animal ***
+    velocidade:1 // velodidade de movimento do animal *** 
+};      
+    //probriendades marcadas com *** são genes ou status que provavelmente serão auterados
+    // de pai pra filhos
 let grupo = []
 
-for(c2=0;c2 < 15 ;c2++){
+for(c2=0;c2 < 15 ;c2++){ // temporario! enche um vetor com copias do obj animal 
     grupo[c2] = Object.create(animal)
 }
-let ids = 0
-let water = [400,300,50,50]
-let velocidade = 30
-let velocidadeswitch = 1
-setInterval(main,velocidade)
+let ids = 0 // ids quantidade de objs no vetor grupo[]
+let water = [400,300,50,50] // temporario relacionado a localização da comida ou agua
+let velocidade = 30 // quantidade de milesegundos do fps não funciona por enquanto 
+let velocidadeswitch = 1 // relacionado ao botão de troca de velecidade não funciona por enquanto 
+setInterval(main,velocidade)// chama e repete a função do main() "principal" basicamente o fps do jogo/simulação
 let testsprite = new Image
 testsprite.src = "1.png"
 let testtree = new Image
